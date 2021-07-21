33c2bb182af54c452157ebac37df8fc8e4fc21ba
tahaTWM <taha.twm.1234@gmail.com>
2021-07-21 06:21:50 +0300

    Wed 07/21/2021,  6:21:50.26

--- github.bat
+++ github.bat
@@ -2,17 +2,17 @@
 git init 
 git add -A
 
-set /p repo="Enter Repo Name : "
-git remote add %repo% https://github.com/tahaTWM/%repo%.git
+rem set /p repo="Enter Repo Name : "
+git remote add origin https://github.com/tahaTWM/taskSupportorWithDarkTheme.git
 
 rem set /p id="Enter Commit Message : "
 set TM=%DATE%, %TIME%
 
 git commit -m "%TM%"
 
-set /p branch="main or master ? "
-if %branch% == main (git branch -M %branch%) 
+rem set /p branch="main or master ? "
+rem if %branch% == main (git branch -M %branch%) 
 
 
-git push -u %repo% %branch% 
+git push -u origin master --force 
 set /p id="it is Done Press any key to close.."
